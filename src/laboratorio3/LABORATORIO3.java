947ac7e17542c3b40a582b402dc7fd9199329e28
angelaAcero <158375313+angelaAcero@users.noreply.github.com>
2024-03-13 09:47:48 -0500

    VERSION INICIAL

---
+++ src/laboratorio3/LABORATORIO3.java
@@ -0,0 +1,26 @@
+        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio3;
+
+/**
+ *
+ * @author amace
+ */
+public class LABORATORIO3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+         CARRERA obj = new CARRERA();
+         obj.setVisible(true);
+         
+         
+    }
+    
+  
+}
